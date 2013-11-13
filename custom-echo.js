8fc7f76c4f571a6fe276480314ad4bbeb76e95d9
wonliao <wonliao@941wan.com.tw>
2013-11-13 23:55:01 +0800

    v0.03

--- custom-echo.js
+++ custom-echo.js
@@ -1,46 +1,20 @@
 
-var sys = require("sys")
-  , fs = require("fs")
-  , path = require("path")
-  , http = require("http")
-  , ws = require(__dirname + '/remy-node-websocket-server-66ca366');
+var http = require("http")
+  , ws = require(__dirname + '/src');
 
-
-function serveFile(req, res){
-  if( req.url.indexOf("favicon") > -1 ){
-
-    res.writeHead(200, {'Content-Type': 'image/x-icon'});
-    res.end("");
-  } else {
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream( path.normalize(path.join(__dirname, "client.html")), {
-      'flags': 'r',
-      'encoding': 'binary',
-      'mode': 0666,
-      'bufferSize': 4 * 1024
-    }).addListener("data", function(chunk){
-      res.write(chunk, 'binary');
-    }).addListener("close",function() {
-      res.end();
-    });
-  }
-};
 
 /*-----------------------------------------------
   Spin up our server:
 -----------------------------------------------*/
-var httpServer = http.createServer(serveFile);
+var httpServer = http.createServer();
 
 
 var connected = 0;
 var server = ws.createServer({
-  debug: true
+	debug: true
 }, httpServer);
 
-server.addListener("listening", function(){
-
-});
+server.addListener("listening", function(){	});
 
 // Handle WebSocket Requests
 server.addListener("connection", function(conn){
